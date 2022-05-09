44418b0a0bc15e23761159510686407bf7cd8bc9
Sam Wozencroft <samwozencroft@hotmail.co.uk>
2022-05-09 17:23:20 +0100

    Adjusted GUI Bottom left & right
    Adjusted GUI Bottom left & right

--- Assets/Scripts/Camera Scripts/GUI_Panel.cs
+++ Assets/Scripts/Camera Scripts/GUI_Panel.cs
@@ -10,8 +10,8 @@ public class GUI_Panel : MonoBehaviour
     {
         /* GUI.Box (new Rect (0,0,100,50), "Top-left"); */
         GUI.Box (new Rect (Screen.width - 100,0,100,50), "Top-right");
-        GUI.Box (new Rect (0,Screen.height - 50,100,50), "Bottom-left");
-        GUI.Box (new Rect (Screen.width - 100,Screen.height - 50,100,50), "Bottom-right");
+        GUI.Box (new Rect (0,Screen.height - 80,750,80), "Bottom-left Unit Hotkeys");
+        GUI.Box (new Rect (Screen.width - 150,Screen.height - 100,150,100), "Bottom-right quick orders");
     }
 
 }
